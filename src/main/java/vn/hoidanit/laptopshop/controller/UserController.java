c8d676e95396873d6162f2175689ae2eb9eb2c25
ĐINH HOÀNG ANH <hoanganhdev.dinh2910@gmail.com>
2024-12-03 06:46:57 +0700

    46-update usercontroller,index.html

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -6,15 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.laptopshop.sevice.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 
-// @Controller //Mo hinh MVC
-// public class UserController {
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello from controller";
-//     }
-// }
-
-@RestController // Mo hinh Restful API
+@Controller // Mo hinh MVC
 public class UserController {
     // DI : dependency injection
     private UserService userService;
@@ -23,8 +15,24 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "index.html";
     }
 }
+
+// @RestController // Mo hinh Restful API
+// public class UserController {
+// // DI : dependency injection
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
